151bc6fca3ebb5e56f2e28f9e48f9569be647f7e
Alex Jacobs <a1603181@myy.haaga-helia.fi>
2018-02-04 20:30:56 +0200

    fixes

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const MongoClient = require('mongodb').MongoClient;
+//var url = "mongodb://localhost:27017/cities";
+let url = "mongodb://heroku_9j6r4fpc:pnglsdaltr7lj578mleg5jjfm9@ds125048.mlab.com:25048/heroku_9j6r4fpc";
+
+MongoClient.connect(url, (err, db) =>
+ {
+  if (err) throw err;
+  let dbo = db.db("heroku_9j6r4fpc");
+  dbo.collection("cities").findOne({}, (err, result) =>
+    {
+        if (err) throw err;
+        console.log(result.name);
+    })
+  console.log("Database connected!");
+  db.close();
+});
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.get('/', (req, res) => 
+{
+    let mongodb_results = [];
+    MongoClient.connect(url, (err, db) =>
+    {
+        if (err) throw err;
+        let dbo = db.db("heroku_9j6r4fpc");
+        let str = dbo.collection("cities").find();
+        str.forEach((doc, err) =>
+        {
+            //console.log(results);
+            //console.log(mongodb_results);
+            mongodb_results.push(doc);
+        }, () =>
+        {
+            console.log(mongodb_results);
+            res.render('index', {results: mongodb_results});
+            db.close();
+        });
+    });
+    
+});
+app.post('/updatetokyo', (req, res) =>
+{
+    let cTemp = req.body.currentTemp;
+    let hTemp = req.body.highestTemp;
+    let lTemp = req.body.lowestTemp;
+    let name = req.body.city;
+    MongoClient.connect(url, (err, db) =>
+    {
+        if (err) throw err;
+        var dbo = db.db("heroku_9j6r4fpc");
+        var myQuery = { name:  name };
+        var newValues = { $set: {currentTemp: cTemp, highTemp: hTemp, lowTemp: lTemp} };
+        dbo.collection("cities").updateOne(myQuery, newValues, (err, res) =>
+        {
+            if (err) throw err;
+            console.log("one document updated");
+            db.close();
+        });
+    });
+    res.redirect("back");
+});
+
+//app.listen(3000, () => console.log('listening on port 3000!'))
